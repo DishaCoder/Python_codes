f1891fe8f1b4a1ed61bfbc03ce8875ef32b38cab
DishaCoder <43693449+DishaCoder@users.noreply.github.com>
2020-06-04 11:35:47 +0530

    Add files via upload
    Specify the time in py file to restart your computer on the desired time.

---
+++ restart_pc.py
@@ -0,0 +1,31 @@
+import os
+from tkinter import *
+from tkinter import messagebox
+def st():
+    os.system("shutdown /r /t 5")
+def nst():
+    exit()
+box = Tk()
+box.geometry('210x80')
+
+lbl = Label(box,text = 'Are you sure want to restart this PC?')
+lbl.grid(column  = 1,row = 1)
+
+yes = Button(box,text = 'Yes',command = st)
+yes.grid(column = 1,row = 2)
+
+no = Button(box,text = 'No',command = nst)
+no.grid(column = 1,row = 3)
+
+
+
+
+
+
+
+'''messagebox.askyesno('Restart','Are you sure want to Restart?')
+check = messagebox.get()
+if check == 'Yes':
+    os.system("shutdown /r /t 5")
+else :
+    exit()'''
